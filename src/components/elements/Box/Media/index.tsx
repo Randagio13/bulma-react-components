ce795227d561d73a92f0454607f99eaef95f70f7
Alessandro Casazza <imasterale@gmail.com>
2019-02-08 01:23:39 +0100

    Added new rules to Media Content

--- src/components/elements/Box/Media/index.tsx
+++ src/components/elements/Box/Media/index.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import Content from '../../Content'
 
 interface Props {
   bgColor?: string
@@ -10,5 +11,11 @@ const Media = styled.article`
   align-items: flex-start;
   display: flex;
   text-align: left;
+  ${Content}:not(:last-child) {
+    margin-bottom: 0.75rem;
+  }
+  @media screen and (max-width: 768px) {
+    overflow-x: auto;
+  }
 `
 export default Media
