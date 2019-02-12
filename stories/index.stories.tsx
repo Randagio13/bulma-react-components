956912a9aa72508cb9f73de6b865cb035c863a31
Alessandro Casazza <imasterale@gmail.com>
2019-02-12 11:08:00 +0100

    Added storybook

---
+++ stories/index.stories.tsx
@@ -0,0 +1,43 @@
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
+import { Box, Image, MediaImage } from '../src'
+import Level from '../src/components/elements/Box/Media/Level'
+import LevelItem from '../src/components/elements/Box/Media/Level/Item'
+import MediaContent from '../src/components/elements/Box/Media/MediaContent'
+import Content from '../src/components/elements/Content'
+import Icon from '../src/components/elements/Icon'
+
+const stories = storiesOf('Elements', module)
+
+stories.add(
+  'Box',
+  () => (
+    <Box>
+      <MediaImage>
+        <Image size='64' src='https://bulma.io/images/placeholders/128x128.png' />
+      </MediaImage>
+      <MediaContent>
+        <Content>
+          <p>
+            <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+            <br/>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+          </p>
+        </Content>
+        <Level isMobile contentPosition='right'>
+          <LevelItem>
+            <Icon size='small' icon='reply' />
+          </LevelItem>
+          <LevelItem>
+            <Icon size='small' icon='retweet' />
+          </LevelItem>
+          <LevelItem>
+            <Icon size='small' icon='heart' />
+          </LevelItem>
+        </Level>
+      </MediaContent>
+    </Box>
+  ),
+  { info: { inline: true } }
+)
