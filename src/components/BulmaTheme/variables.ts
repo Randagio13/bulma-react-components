aa662b315397c215094d03d237e23c847b16c290
Alessandro Casazza <imasterale@gmail.com>
2019-08-12 22:15:12 +0200

    Added colors variables

---
+++ src/components/BulmaTheme/variables.ts
@@ -0,0 +1,21 @@
+export default {
+  // Colors
+  $black: 'hsl(0, 0%, 4%)',
+  $blackBis: 'hsl(0, 0%, 7%)',
+  $blackTer: 'hsl(0, 0%, 14%)',
+  $greyDarker: 'hsl(0, 0%, 21%)',
+  $greyDark: 'hsl(0, 0%, 29%)',
+  $grey: 'hsl(0, 0%, 48%)',
+  $greyLight: 'hsl(0, 0%, 71%)',
+  $greyLighter: 'hsl(0, 0%, 86%)',
+  $whiteTer: 'hsl(0, 0%, 96%)',
+  $whiteBis: 'hsl(0, 0%, 98%)',
+  $white: 'hsl(0, 0%, 100%)',
+  $orange: 'hsl(14,  100%, 53%)',
+  $yellow: 'hsl(48,  100%, 67%)',
+  $green: 'hsl(141, 71%,  48%)',
+  $turquoise: 'hsl(171, 100%, 41%)',
+  $blue: 'hsl(217, 71%,  53%)',
+  $purple: 'hsl(271, 100%, 71%)',
+  $red: 'hsl(348, 100%, 61%)'
+}
