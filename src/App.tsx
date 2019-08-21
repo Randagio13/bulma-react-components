f8fd0311a512167505528a3d4ac54fd061ed5fe1
Alessandro Casazza <imasterale@gmail.com>
2019-08-21 19:06:45 +0200

    Remove default code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react'
-import logo from './logo.svg'
-import './App.scss'
+// import logo from './logo.svg'
+// import './App.scss'
 import BulmaTheme from './components/BulmaTheme'
 import Button from './components/Button/Button'
 import { ThemeType } from './components/BulmaTheme/BulmaTheme'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  padding: 1rem;
+`
 
 console.log(Text)
 
@@ -20,30 +25,42 @@ const App: React.FC = () => {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <BulmaTheme theme={theme}>
-          <Button color='primary'>cioa tea</Button>
-        </BulmaTheme>
-        <Button color='warning'>Button</Button>
-        <Button>Button</Button>
-        <Button color='primary' type='a'>
-          without theme
-        </Button>
-        <p>
+        {/* <img src={logo} className='App-logo' alt='logo' /> */}
+        <Container>
+          <BulmaTheme theme={theme}>
+            <Button color='primary'>Button with theme</Button>
+          </BulmaTheme>
+        </Container>
+        <Container>
+          <Button color='warning'>Button</Button>
+        </Container>
+        <Container>
+          <Button>Button</Button>
+        </Container>
+        <Container>
+          <Button color='primary' type='a'>
+            Button Type a
+          </Button>
+        </Container>
+        {/* <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button type='submit'>Submit input without theme</Button>
-        <a
+        </p> */}
+        <Container>
+          <Button type='submit'>Submit input without theme</Button>
+        </Container>
+        {/* <a
           className='App-link'
           href='https://reactjs.org'
           target='_blank'
           rel='noopener noreferrer'
         >
           Learn React
-        </a>
-        <Button color='primary' type='reset'>
-          Reset input without theme
-        </Button>
+        </a> */}
+        <Container>
+          <Button color='primary' type='reset'>
+            Reset input without theme
+          </Button>
+        </Container>
       </header>
     </div>
   )
